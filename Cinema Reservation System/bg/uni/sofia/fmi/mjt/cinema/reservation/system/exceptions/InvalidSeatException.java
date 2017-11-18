1388d07e034df27c271faf5f8f56455e1d0cb7b6
Martin Vrachev <martin.vrachev@gmail.com>
2017-11-18 22:51:13 +0200

    Delete InvalidSeatException.java

--- Cinema Reservation System/bg/uni/sofia/fmi/mjt/cinema/reservation/system/exceptions/InvalidSeatException.java
+++
@@ -1,7 +0,0 @@
-package bg.uni.sofia.fmi.mjt.cinema.reservation.system.exceptions;
-
-public class InvalidSeatException extends Exception {
-
-	public InvalidSeatException() { super();}
-	public InvalidSeatException(String message) { super(message); }
-}
