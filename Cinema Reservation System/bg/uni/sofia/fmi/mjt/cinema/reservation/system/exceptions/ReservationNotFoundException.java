aebd07d5d6508eb8c9cee582ee6dba25cf2b583e
Martin Vrachev <martin.vrachev@gmail.com>
2017-11-18 22:51:19 +0200

    Delete ReservationNotFoundException.java

--- Cinema Reservation System/bg/uni/sofia/fmi/mjt/cinema/reservation/system/exceptions/ReservationNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package bg.uni.sofia.fmi.mjt.cinema.reservation.system.exceptions;
-
-public class ReservationNotFoundException extends Exception {
-
-	public ReservationNotFoundException() { super();}
-	public ReservationNotFoundException(String message) { super(message); }
-}
