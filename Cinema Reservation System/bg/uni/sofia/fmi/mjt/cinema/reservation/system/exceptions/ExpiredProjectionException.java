c45069e31c0b4e624539df19d3f5b9aa1b7e7190
Martin Vrachev <martin.vrachev@gmail.com>
2017-11-18 22:51:07 +0200

    Delete ExpiredProjectionException.java

--- Cinema Reservation System/bg/uni/sofia/fmi/mjt/cinema/reservation/system/exceptions/ExpiredProjectionException.java
+++
@@ -1,7 +0,0 @@
-package bg.uni.sofia.fmi.mjt.cinema.reservation.system.exceptions;
-
-public class ExpiredProjectionException extends Exception{
-
-	public ExpiredProjectionException() { super();}
-	public ExpiredProjectionException(String message) { super(message); }
-}
